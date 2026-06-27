fa17d9761dc2102daf9707a65bbd6990a9d23ffa
Cristian Cayun <cristian.cayun@bostontech.cl>
2026-06-27 16:23:34 -0400

    first commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   printerPath: "\\\\localhost\\POS-80",   // Recurso compartido de Windows
   pollingUrl: "https://cafeteriavelora.cl/cliente_mesas_obtener_imprimir",  // URL a consultar
-  pollingInterval: 10000,                  // milisegundos entre consultas
+  pollingInterval: 3000,                  // milisegundos entre consultas
   deviceId: "mi-impresora-01",            // Identificador para la API
   encoding: "UTF-8"                     // Codificación para caracteres especiales
 };
